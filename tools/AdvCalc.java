8045826531dd7fbf7c69179641679d7b8e53e9cd
Amit Anand <amit00007anand@gmail.com>
2024-12-26 00:37:09 +0530

    Add files via upload

---
+++ tools/AdvCalc.java
@@ -0,0 +1,12 @@
+package tools;
+
+public class AdvCalc extends calc{
+    // here AdvCalc is sub class(child class) which is using the features of super class(parent class)
+    // this is single inheritance
+    public int mul(int n1, int n2){
+        return n1 * n2;
+    }
+    public int div(int n1, int n2){
+        return n1 / n2;
+    }
+}
