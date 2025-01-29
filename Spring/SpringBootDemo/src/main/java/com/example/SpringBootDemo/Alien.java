25980039dc2df04a5df6fdf672d9bb7dfac810ab
Amit Anand <amit00007anand@gmail.com>
2025-01-29 13:38:42 +0530

    Comments added.

--- Spring/SpringBootDemo/src/main/java/com/example/SpringBootDemo/Alien.java
+++ Spring/SpringBootDemo/src/main/java/com/example/SpringBootDemo/Alien.java
@@ -4,9 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+    /*
+    the @Component annotation is used to mark a Java class as a Spring-managed bean. When Spring's 
+    component-scanning mechanism detects a class annotated with @Component, it automatically registers 
+    the class as a bean in the Spring ApplicationContext.
+    */
 public class Alien {
 
     @Autowired
+    // simplifies dependency injection by allowing Spring to resolve and inject beans automatically.
     Laptop laptop;
 
     public void code() {
