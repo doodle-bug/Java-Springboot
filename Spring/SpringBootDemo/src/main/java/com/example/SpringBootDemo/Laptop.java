db78edcaea4ef64067c831174fc49194ea45e6bb
Amit Anand <amit00007anand@gmail.com>
2025-01-29 13:39:10 +0530

    Comments added.

--- Spring/SpringBootDemo/src/main/java/com/example/SpringBootDemo/Laptop.java
+++ Spring/SpringBootDemo/src/main/java/com/example/SpringBootDemo/Laptop.java
@@ -3,6 +3,11 @@ package com.example.SpringBootDemo;
 import org.springframework.stereotype.Component;
 
 @Component
+    /*
+    the @Component annotation is used to mark a Java class as a Spring-managed bean. When Spring's 
+    component-scanning mechanism detects a class annotated with @Component, it automatically registers 
+    the class as a bean in the Spring ApplicationContext.
+    */
 public class Laptop {
     public void compile(){
         System.out.println("Compiling...");
