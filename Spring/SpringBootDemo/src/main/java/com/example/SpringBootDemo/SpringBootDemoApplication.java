c8441926bf8cd0e7c56fcbc91c5fb444cd7de0df
Amit Anand <amit00007anand@gmail.com>
2025-01-29 13:30:52 +0530

    Comments added.

--- Spring/SpringBootDemo/src/main/java/com/example/SpringBootDemo/SpringBootDemoApplication.java
+++ Spring/SpringBootDemo/src/main/java/com/example/SpringBootDemo/SpringBootDemoApplication.java
@@ -9,8 +9,15 @@ public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SpringBootDemoApplication.class, args);
+		// Application context is core interface in spring's IoC container and holds beans with their configuration.
+		/* SpringApplication.run() serves as entry point for springboot framework. It sets up the application context
+			including scanning and registering beans */
+		
+		
 //		System.out.println("Spring trial.");
 		Alien obj = context.getBean(Alien.class);
+		// .getBean() method is used to retrieve beans from the Spring container by name, type, or both.
+		
 		obj.code();
 	}
 }
