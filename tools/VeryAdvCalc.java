03dbcff9b18b4adf8fdeb21d17da630cab7d98a9
Amit Anand <amit00007anand@gmail.com>
2026-06-27 19:14:44 +0530

    Delete tools directory

--- tools/VeryAdvCalc.java
+++
@@ -1,8 +0,0 @@
-package tools;
-
-public class VeryAdvCalc extends AdvCalc {
-    // this is multi level inheritance
-    public double power(int n1, int n2){
-        return Math.pow(n1, n2);
-    }
-}
